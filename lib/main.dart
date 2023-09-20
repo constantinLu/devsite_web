7434fd20ba7f840a1a074af123f06381d227fce1
Constantin Lungu <lunguucatalin@gmail.com>
2023-09-20 17:01:03 +0300

    DEV-13 - fixed static on pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:devsite_web/presentation/app_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
